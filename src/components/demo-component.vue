5b5c4f1ea431830b5f9cd790d712748a6cf33cbc
Dexter Lesaca <emobile@gmail.com>
2020-03-17 16:30:52 -0700

    initial setup and add web components

---
+++ src/components/demo-component.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ods-button>Toast</ods-button>
+    <ods-inputoptions
+        ref={inputOptionsRef}
+        id="rdo"
+        type="checkbox"
+        name="rdo"
+        label="This is a label"
+        onInput={handleInput}
+        for="radio1" componentData="[]"></ods-inputoptions>
+    <ods-button @click="alertApp">Change Toaster</ods-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoComponent',
+  label: 'Your Choice',
+  methods: {
+    alertApp() {
+      console.log('Alert'); 
+    }
+  }
+}
+</script>
