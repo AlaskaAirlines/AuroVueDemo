1dd0158299e449f3ee3ff639d8d626359c151208
Dexter Lesaca <emobile@gmail.com>
2020-03-18 13:40:01 -0700

    add button functionality

--- src/components/demo-component.vue
+++ src/components/demo-component.vue
@@ -1,26 +1,44 @@
 <template>
   <div>
-    <ods-button>Toast</ods-button>
+    <ods-button @click="handleClick">Toast</ods-button>
     <ods-inputoptions
         ref={inputOptionsRef}
         id="rdo"
         type="checkbox"
         name="rdo"
-        label="This is a label"
+        label="Static label"
         onInput={handleInput}
         for="radio1" componentData="[]"></ods-inputoptions>
-    <ods-button @click="alertApp">Change Toaster</ods-button>
+    <ods-button @click="updateToastMsg">Change Toaster</ods-button>
   </div>
 </template>
 
 <script>
+import Swipe from "@alaskaairux/ods-toast/dist/swipe.js";
+import Toaster from "@alaskaairux/ods-toast/dist/toaster";
+
+window.Swipe = Swipe;
+const toaster = new Toaster();
+
 export default {
   name: 'DemoComponent',
   label: 'Your Choice',
+  data: function() {
+    return {
+      toastMsg: "Hello!"
+    }
+  },
   methods: {
-    alertApp() {
-      console.log('Alert'); 
+    handleClick() {
+      toaster.add(this.toastMsg);
+    },
+    updateToastMsg() {
+      this.toastMsg = "Hola!";
     }
   }
 }
 </script>
+
+<style scoped>
+@import "../../node_modules/@alaskaairux/ods-toast/dist/toaster.css";
+</style>
