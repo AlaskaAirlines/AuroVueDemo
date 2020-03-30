b19f981a5389464a8a2f9f23451c19c04388a8dd
Dale Sande <dale.sande@alaskaair.com>
2020-03-30 11:16:04 -0700

    feature update to button action

--- src/components/demo-component.vue
+++ src/components/demo-component.vue
@@ -9,7 +9,8 @@
         @input="handleInput"
         for="cbx"
         componentData='[]'></ods-inputoptions>
-    <ods-button class="Toaster-btn" @click="updateToastMsg">Change Toaster</ods-button>
+    <ods-button class="Toaster-btn" @click="updateToastMsg">Change Toast Message: {{this.toastMsg}}</ods-button>
+    <ods-button class="Toaster-btn" @click="handleClick">Show me a Toast</ods-button>
   </div>
 </template>
 
